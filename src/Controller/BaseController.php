a129c751b36ee3b503955975f7e424ee33a81246
CodeMonkeyJeffGT <18846938586@163.com>
2018-06-25 00:03:35 +0800

    demo

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -12,7 +12,7 @@ class BaseController extends Controller
      */
     public function index()
     {
-        return $this->render('base/index.html', array(
+        return $this->render('base/doc.html', array(
             'tree' => $this->getConfig()
         ));
     }
@@ -36,5 +36,39 @@ class BaseController extends Controller
                 'url' => $this->generateUrl('doc-multiselect'),
             ),
         );
+    }
+
+    /**
+     * 数组转换为js数组
+     */
+    protected function toJs($arr, $level = 1)
+    {
+        $tpl = "{\n%s},";
+        if (array_values($arr) == $arr) {
+            $tpl = "[\n%s],";
+        }
+        $str = "";
+        foreach($arr as $key => $value) {
+            if (is_array($value)) {
+                $value = $this->toJs($value, $level + 1);
+            } elseif (is_numeric($value)) {
+                $value = $value . ",";
+            } else {
+                $value = "\"" . $value . "\",";
+            }
+            if ($tpl != "{\n%s},") {
+                $str .= str_repeat(" ", $level * 4) . $value . "\n";
+            } else {
+                $str .= str_repeat(" ", $level * 4) . $key . ": " . $value . "\n";
+            }
+        }
+        $loc = strrpos($str, ',');
+        $str = substr($str, 0, $loc) . substr($str, $loc + 1);
+        $str .= str_repeat(" ", $level * 4 - 4);
+        $str = sprintf($tpl, $str);
+        if ($level == 1) {
+            $str = substr($str, 0, strlen($str) - 1);
+        }
+        return $str;
     }
 }
--- src/Controller/Menu/BaseController.php
+++ src/Controller/Menu/BaseController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Controller\Menu;
 
 use Symfony\Component\Routing\Annotation\Route;
@@ -7,13 +6,48 @@ use App\Controller\BaseController as Controller;
 
 class BaseController extends Controller
 {
+    public function index()
+    {
+        return $this->render('base/api.html', $this->getConfig());
+    }
     protected function getConfig()
     {
         return array(
-            array(
-                'name' => '../',
-                'url' => $this->generateUrl('doc-index'),
-            )
+            'uri' => '/menu',
+            'params' => array(
+                array(
+                    'name' => 'key',
+                    'type' => 'string',
+                    'default' => '0',
+                    'must' => '',
+                ),
+                array(
+                    'name' => 'name',
+                    'type' => 'string',
+                    'default' => '',
+                    'must' => '是',
+                ),
+            ),
+            'return' => $this->toJs(array(
+                'uri' => 'menu',
+                'params' => array(
+                    array(
+                        'name' => 'key',
+                        'type' => 'string',
+                        'default' => '0',
+                        'must' => true,
+                    ),
+                    array(
+                        'name' => 'key',
+                        'type' => 'string',
+                        'default' => '0',
+                        'must' => true,
+                    )
+                ),
+                'return' => array(
+
+                )
+            )),
         );
     }
 }
